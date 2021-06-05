f9dd7f4df6ab0efd359189a9dcd83f86601255bd
David Brochart <david.brochart@gmail.com>
2021-06-05 21:41:07 +0200

    Implement kernel interruption

--- akernel/kernel.py
+++ akernel/kernel.py
@@ -1,6 +1,5 @@
 import sys
 import platform
-import signal
 import asyncio
 import json
 import traceback
@@ -16,13 +15,6 @@ from ._version import __version__
 
 
 DELIM = b"<IDS|MSG>"
-
-
-def signal_handler(sig, frame):
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
 
 
 def __print__(kernel):
@@ -48,6 +40,10 @@ def __print__(kernel):
         send_message(msg, kernel.iopub_channel, kernel.key)
 
     return new_print
+
+
+async def check_message(sock: Socket) -> bool:
+    return await sock.poll(0)
 
 
 async def receive_message(
@@ -78,8 +74,12 @@ def make_async(code: str, globals_: Dict[str, Any]) -> str:
         "        __print__(*objects, sep=sep, end=end, file=file, flush=flush, "
         "parent_header=__parent_header__)"
     ]
+    async_code += ["    __result__ = None"]
+    async_code += ["    __exception__ = None"]
+    async_code += ["    __interrupted__ = False"]
+    async_code += ["    try:"]
     code_lines = code.splitlines()
-    async_code += ["    " + line for line in code_lines[:-1]]
+    async_code += ["        " + line for line in code_lines[:-1]]
     last_line = code_lines[-1]
     return_value = False
     if not last_line.startswith((" ", "\t")):
@@ -91,15 +91,25 @@ def make_async(code: str, globals_: Dict[str, Any]) -> str:
             if type(n.body[0]) is ast.Expr:
                 return_value = True
     if return_value:
-        async_code += ["    __result__ = " + last_line]
+        async_code += ["        __result__ = " + last_line]
     else:
-        async_code += ["    " + last_line]
-        async_code += ["    __result__ = None"]
+        async_code += ["        " + last_line]
+    async_code += ["    except asyncio.CancelledError:"]
+    async_code += ["        __exception__ = RuntimeError('Kernel interrupted')"]
+    async_code += ["    except KeyboardInterrupt:"]
+    async_code += ["        __exception__ = RuntimeError('Kernel interrupted')"]
+    async_code += ["        __interrupted__ = True"]
+    async_code += ["    except Exception as e:"]
+    async_code += ["        __exception__ = e"]
     async_code += ["    __globals__.update(locals())"]
     async_code += ["    __globals__.update(globals())"]
     async_code += ["    del __globals__['print']"]
     async_code += ["    del __globals__['__parent_header__']"]
-    async_code += ["    return __result__"]
+    async_code += ["    del __globals__['__result__']"]
+    async_code += ["    del __globals__['__exception__']"]
+    async_code += ["    if __exception__ is None:"]
+    async_code += ["        return __result__"]
+    async_code += ["    raise __exception__"]
     return "\n".join(async_code)
 
 
@@ -109,10 +119,13 @@ class Kernel:
         kernel_name: str,
         connection_file: str,
     ):
+        self.loop = asyncio.get_event_loop()
         self.kernel_name = kernel_name
+        self.running_cells = {}
+        self.task_i = 0
         self.execution_count = 1
         self.execution_state = "starting"
-        self.globals = {}
+        self.globals = {"__interrupted__": False}
         self.global_context = {
             "asyncio": asyncio,
             "sys": sys,
@@ -120,24 +133,36 @@ class Kernel:
             "__globals__": self.globals,
         }
         self.local_context = {}
-        self.parent_header = {}
         with open(connection_file) as f:
             self.connection_cfg = json.load(f)
         self.key = cast(str, self.connection_cfg["key"])
-        asyncio.run(self.main())
+        init = True
+        while True:
+            try:
+                self.loop.run_until_complete(self.main(init))
+            except KeyboardInterrupt:
+                for task in self.running_cells.values():
+                    task.cancel()
+                self.running_cells = {}
+            else:
+                if not self.restart:
+                    break
+            finally:
+                init = False
 
-    async def main(self):
-        self.shell_channel = connect_channel("shell", self.connection_cfg)
-        self.iopub_channel = connect_channel("iopub", self.connection_cfg)
-        self.control_channel = connect_channel("control", self.connection_cfg)
-        msg = create_message(
-            "status", content={"execution_state": self.execution_state}
-        )
-        send_message(msg, self.iopub_channel, self.key)
-        self.execution_state = "idle"
-        self.stop = asyncio.Event()
-        asyncio.create_task(self.listen_shell())
-        asyncio.create_task(self.listen_control())
+    async def main(self, init):
+        if init:
+            self.shell_channel = connect_channel("shell", self.connection_cfg)
+            self.iopub_channel = connect_channel("iopub", self.connection_cfg)
+            self.control_channel = connect_channel("control", self.connection_cfg)
+            msg = create_message(
+                "status", content={"execution_state": self.execution_state}
+            )
+            send_message(msg, self.iopub_channel, self.key)
+            self.execution_state = "idle"
+            self.stop = asyncio.Event()
+            asyncio.create_task(self.listen_shell())
+            asyncio.create_task(self.listen_control())
         while True:
             await self.stop.wait()
             if self.restart:
@@ -147,6 +172,10 @@ class Kernel:
 
     async def listen_shell(self):
         while True:
+            if self.globals["__interrupted__"] and not await check_message(
+                self.shell_channel
+            ):
+                self.globals["__interrupted__"] = False
             idents, msg = await receive_message(self.shell_channel)
             msg_type = msg["header"]["msg_type"]
             parent_header = msg["header"]
@@ -176,6 +205,9 @@ class Kernel:
                 )
                 send_message(msg, self.iopub_channel, self.key)
             elif msg_type == "execute_request":
+                if self.globals["__interrupted__"]:
+                    self.finish_execution(idents, parent_header, no_exec=True)
+                    continue
                 self.execution_state = "busy"
                 code = msg["content"]["code"]
                 msg = create_message(
@@ -190,7 +222,11 @@ class Kernel:
                 except Exception as e:
                     self.finish_execution(idents, parent_header, exception=e)
                 else:
-                    asyncio.create_task(self.execute_code(idents, parent_header))
+                    task = asyncio.create_task(
+                        self.execute_code(idents, parent_header, self.task_i)
+                    )
+                    self.running_cells[self.task_i] = task
+                    self.task_i += 1
 
     async def listen_control(self):
         while True:
@@ -214,14 +250,19 @@ class Kernel:
                         "__globals__": self.globals,
                     }
                     self.local_context = {}
+                    self.execution_count = 1
                 self.stop.set()
 
-    async def execute_code(self, idents, parent_header):
+    async def execute_code(self, idents, parent_header, task_i):
         exception = None
         try:
             result = await self.local_context["__async_cell__"](parent_header)
         except Exception as e:
             exception = e
+            if self.globals["__interrupted__"]:
+                for task in self.running_cells.values():
+                    task.cancel()
+                self.running_cells = {}
         else:
             if result is not None:
                 msg = create_message(
@@ -233,19 +274,33 @@ class Kernel:
         finally:
             self.global_context.update(self.globals)
             self.finish_execution(idents, parent_header, exception=exception)
+            if task_i in self.running_cells:
+                del self.running_cells[task_i]
 
-    def finish_execution(self, idents, parent_header, exception=None):
-        if exception is None:
+    def finish_execution(self, idents, parent_header, exception=None, no_exec=False):
+        execution_count = self.execution_count
+        if no_exec:
             status = "ok"
+            execution_count = None
         else:
-            status = "error"
-            tb = "".join(traceback.format_tb(exception.__traceback__))
-            msg = create_message(
-                "stream",
-                parent_header=parent_header,
-                content={"name": "stderr", "text": f"{tb}{exception}\n"},
-            )
-            send_message(msg, self.iopub_channel, self.key)
+            self.execution_count += 1
+            if exception is None:
+                status = "ok"
+            else:
+                status = "error"
+                tb = "".join(traceback.format_tb(exception.__traceback__))
+                msg = create_message(
+                    "stream",
+                    parent_header=parent_header,
+                    content={"name": "stderr", "text": f"{tb}{exception}\n"},
+                )
+                send_message(msg, self.iopub_channel, self.key)
+        msg = create_message(
+            "execute_reply",
+            parent_header=parent_header,
+            content={"status": status, "execution_count": execution_count},
+        )
+        send_message(msg, self.shell_channel, self.key, idents[0])
         self.execution_state = "idle"
         msg = create_message(
             "status",
@@ -253,10 +308,3 @@ class Kernel:
             content={"execution_state": self.execution_state},
         )
         send_message(msg, self.iopub_channel, self.key)
-        msg = create_message(
-            "execute_reply",
-            parent_header=parent_header,
-            content={"status": status, "execution_count": self.execution_count},
-        )
-        send_message(msg, self.shell_channel, self.key, idents[0])
-        self.execution_count += 1
